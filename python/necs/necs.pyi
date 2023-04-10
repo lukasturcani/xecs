7d743b0017762cd2130711c36cf68ad8fe4aaafe
Lukas Turcani <lukasturcani93@gmail.com>
2023-04-10 14:11:26 +0100

    Remove old files

--- python/necs/necs.pyi
+++ python/necs/necs.pyi
@@ -3,17 +3,19 @@ import typing
 import numpy as np
 import numpy.typing as npt
 
-from necs._internal.element_type import ElementType
-
-numpy_ints: typing.TypeAlias = (
-    np.int8
-    | np.int16
-    | np.int32
-    | np.int64
-    | np.uint8
-    | np.uint16
-    | np.uint32
-    | np.uint64
+ElementType = typing.TypeVar(
+    "ElementType",
+    np.bool_,
+    np.int8,
+    np.int16,
+    np.int32,
+    np.int64,
+    np.uint8,
+    np.uint16,
+    np.uint32,
+    np.uint64,
+    np.float32,
+    np.float64,
 )
 
 ViewType = typing.TypeVar("ViewType")
@@ -24,9 +26,7 @@ class _Array(typing.Generic[ElementType, ViewType]):
     def numpy(self) -> npt.NDArray[ElementType]: ...
     def view(self) -> ViewType: ...
 
-Key: typing.TypeAlias = (
-    list[int] | tuple[int, ...] | npt.NDArray[numpy_ints | np.bool_] | slice
-)
+Key: typing.TypeAlias = npt.NDArray[np.uint64 | np.bool_] | slice
 
 class ArrayViewF64:
     def __getitem__(self, key: Key) -> typing.Self:
