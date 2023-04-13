6fc41980e8575f23e96978d1a1015f4ae813f59b
Lukas Turcani <lukasturcani93@gmail.com>
2023-04-13 14:51:31 +0100

    test passing

--- tests/test_component.py
+++ tests/test_component.py
@@ -26,11 +26,37 @@ class MyComponent(ecs.Component):
 def test_spawning_entities_updates_views_of_children() -> None:
     pool = MyComponent.create_pool(100)
     pool.p_spawn(50)
-    component = pool.p_component
 
+    component = pool.p_component
     assert len(component) == 50
+    assert len(component.d) == 50
+    assert len(component.e) == 50
+    assert len(component.f) == 50
+    assert len(component.e.a) == 50
+    assert len(component.f.b) == 50
+    assert len(component.f.c) == 50
+    assert len(component.f.c.a) == 50
+
     sub_view = component[indices([0, 10, 32])]
-    assert len(component) == 50
+    assert len(sub_view) == 3
+    assert len(sub_view.d) == 3
+    assert len(sub_view.e) == 3
+    assert len(sub_view.f) == 3
+    assert len(sub_view.e.a) == 3
+    assert len(sub_view.f.b) == 3
+    assert len(sub_view.f.c) == 3
+    assert len(sub_view.f.c.a) == 3
+
+    pool.p_spawn(50)
+    assert len(component) == 100
+    assert len(component.d) == 100
+    assert len(component.e) == 100
+    assert len(component.f) == 100
+    assert len(component.e.a) == 100
+    assert len(component.f.b) == 100
+    assert len(component.f.c) == 100
+    assert len(component.f.c.a) == 100
+
     assert len(sub_view) == 3
     assert len(sub_view.d) == 3
     assert len(sub_view.e) == 3
