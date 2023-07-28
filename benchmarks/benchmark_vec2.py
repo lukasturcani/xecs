220652591efb706b7aed463c2f13d71219226356
Lukas Turcani <lukasturcani93@gmail.com>
2023-07-28 20:59:16 +0100

    change benchmark

--- benchmarks/benchmark_vec2.py
+++ benchmarks/benchmark_vec2.py
@@ -9,10 +9,6 @@ import xecs as xx
 @pytest.mark.benchmark(group="Vec2.__mul__")
 def benchmark_mul(benchmark: typing.Any) -> None:
     generator = np.random.default_rng(55)
-    first = xx.Vec2.from_numpy(
-        generator.random((2, 1_000_000), dtype=np.float32)
-    )
-    second = xx.Float32.p_from_numpy(
-        generator.random(1_000_000, dtype=np.float32)
-    )
+    first = xx.Vec2.from_numpy(generator.random((2, 1_000), dtype=np.float32))
+    second = xx.Float32.p_from_numpy(generator.random(1_000, dtype=np.float32))
     benchmark(operator.mul, first, second)
