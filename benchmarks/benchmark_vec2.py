8759c1e2366bfceff01cabd7902013b77316026f
Lukas Turcani <lukasturcani93@gmail.com>
2023-07-28 20:58:27 +0100

    Change benchmark

--- benchmarks/benchmark_vec2.py
+++ benchmarks/benchmark_vec2.py
@@ -12,7 +12,7 @@ def benchmark_mul(benchmark: typing.Any) -> None:
     first = xx.Vec2.from_numpy(
         generator.random((2, 1_000_000), dtype=np.float32)
     )
-    second = xx.Vec2.from_numpy(
-        generator.random((2, 1_000_000), dtype=np.float32)
+    second = xx.Float32.p_from_numpy(
+        generator.random(1_000_000, dtype=np.float32)
     )
     benchmark(operator.mul, first, second)
