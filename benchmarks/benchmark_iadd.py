e40a9e6a90fa9b29baf70c452bb26de8b9af3bfd
Lukas Turcani <lukasturcani93@gmail.com>
2023-10-11 10:24:37 +0100

    Add benchmarks

--- benchmarks/benchmark_iadd.py
+++ benchmarks/benchmark_iadd.py
@@ -2,6 +2,7 @@ import operator
 import typing
 
 import numpy as np
+import polars as pl
 import pytest
 import xecs as xx
 
@@ -38,3 +39,16 @@ def benchmark_iadd_xecs(benchmark: typing.Any, size: int) -> None:
     first = xx.Float32.p_from_numpy(generator.random(size, dtype=np.float32))
     second = xx.Float32.p_from_numpy(generator.random(size, dtype=np.float32))
     benchmark(operator.iadd, first, second)
+
+
+@pytest.mark.benchmark(group="polars-iadd")
+def benchmark_iadd_polars(benchmark: typing.Any, size: int) -> None:
+    generator = np.random.default_rng(55)
+    first = generator.random(size, dtype=np.float32)
+    second = generator.random(size, dtype=np.float32)
+    df = pl.DataFrame({"a": first, "b": second})
+    benchmark(polars_add, df)
+
+
+def polars_add(df: pl.DataFrame) -> pl.DataFrame:
+    return df.select((pl.col("a") + pl.col("b")))
--- benchmarks/benchmark_iadd_mask.py
+++ benchmarks/benchmark_iadd_mask.py
@@ -2,6 +2,7 @@ import typing
 
 import numpy as np
 import numpy.typing as npt
+import polars as pl
 import pytest
 import xecs as xx
 
@@ -48,6 +49,19 @@ def benchmark_iadd_xecs(
     benchmark(iadd_xecs, first, first_key, second[first_key])
 
 
+@pytest.mark.benchmark(group="polars-iadd-mask")
+def benchmark_iadd_polars(
+    benchmark: typing.Any,
+    size: int,
+    key_size: float,
+) -> None:
+    generator = np.random.default_rng(55)
+    first = generator.random(size, dtype=np.float32)
+    second = generator.random(size, dtype=np.float32)
+    df = pl.DataFrame({"a": first, "b": second})
+    benchmark(iadd_polars, df, key_size)
+
+
 def iadd_numpy(
     first: npt.NDArray[np.float32],
     first_key: npt.NDArray[np.bool_],
@@ -63,6 +77,12 @@ def iadd_xecs(
 ) -> None:
     first_subview = first[first_key]
     first_subview += second
+
+
+def iadd_polars(df: pl.DataFrame, key_size: int) -> pl.DataFrame:
+    return df.filter(pl.col("a") < key_size).with_columns(
+        pl.col("a") + pl.col("b")
+    )
 
 
 @pytest.fixture(
